eb1bf05679e57aadb747315c8921b597f0cdd513
Jitendriya-Meher <jiten@gcekbpatna.ac.in>
2024-12-20 00:40:42 +0530

    adding labs

--- lab 1/main2.py
+++ lab 1/main2.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 
 # url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/breast-cancer-wisconsin/wdbc.data'
 
